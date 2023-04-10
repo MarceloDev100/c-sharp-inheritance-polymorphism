1127a295c9290988b31ea80aada40d33d320f213
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-09 23:15:29 -0300

    Inheritance - Account and BusinessAccount classes

---
+++ Project/Course/Course/Program.cs
@@ -0,0 +1,8 @@
+﻿using Course.Entities;
+
+BusinessAccount account = new BusinessAccount(8010, "Bob", 100.0, 500.00);
+Console.WriteLine(account.Balance);
+
+// Balance has protected property, so it can only be changed in Account subclasses.
+// Classes doesn't have permission to set it.
+//account.Balance = 300.00;  // error
