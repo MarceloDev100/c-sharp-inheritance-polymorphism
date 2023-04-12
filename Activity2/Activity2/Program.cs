ff30d1694efc1e637da392adb951f5f13bdcf332
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-12 20:59:56 -0300

    Activity2

---
+++ Activity2/Activity2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Activity2.Entities;
+using System.Globalization;
+
+Console.Write("Enter the number of products: ");
+int n = int.Parse(Console.ReadLine());
+
+List<Product> products = new List<Product>();
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Product #{i} data: ");
+    Console.Write("Common, used or imported (c/u/i)? ");
+    char answer = char.Parse(Console.ReadLine().ToLower());
+
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    if(answer == 'c')
+    {
+        products.Add(new Product(name, price));
+    }
+    else if (answer == 'i')
+    {
+        Console.Write("Customs fee : ");
+        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        products.Add(new ImportedProduct(name, price, customsFee));
+    }
+    else if(answer == 'u')
+    {
+        Console.Write("Manufacture Date (DD/MM/YYYY): ");
+        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+        products.Add(new UsedProduct(name, price, manufactureDate));    
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("PRICE TAGS: ");
+foreach(Product product in products)
+{
+    Console.WriteLine(product.PriceTag());
+}
---
+++ Activity2/README.md
@@ -0,0 +1,7 @@
+## C# .NET6.0 - Activity 2
+
+
+Write a program to read the data of N products (N provided by the user). At the end, show the price tag of each product in the same order in which they were typed.
+
+
+Every product has a name and price. Imported products have a customs fee, and used products have a manufacturing date. The specific data must be in the price tag. For imported products, customs fee must be added to the final price of the product.
